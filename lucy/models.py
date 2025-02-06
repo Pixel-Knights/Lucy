6e1e679e5f54192b17a6e04dc8f24394cba7208a
jonasdasneves <jonas.joliveira2024@gmail.com>
2025-02-05 23:30:47 -0300

    Atualização do front

--- lucy/models.py
+++ lucy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
+
+class System_config(models.Model):
+
+    config_content = models.CharField(max_length=200,null=False,blank=False)
+    config_date= models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self) -> str:
+        return self.nome
+
 
 # Create your models here.
--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,53 @@ function enviarMensagem(){ //Função para enviar a mensagem, por enquanto vai a
     let mensagem = document.getElementById("mensager").value;
     console.log(mensagem);
 }
+
+// [ARMAZENAR VIRIÁVEL NO HTML ]
+
+document.addEventListener('DOMContentLoaded', () => {
+    let contador = 0; // Inicializa um contador para gerar IDs únicos para as tarefas
+
+    // Define uma ação ao clicar no botão "Adicionar Tarefa" 
+    document.getElementById('button').onclick = function() {
+
+        // Captura o valor inserido no campo de texto
+        let mensagem = document.getElementById('mensager').value;
+
+        // Verifica se a tarefa não está vazia
+        if (mensagem !== "") {
+            contador++; // Incrementa o contador para o próximo ID único
+            let divMensagens = document.getElementById('messages'); // Seleciona a DIV onde as tarefas serão exibidas
+
+            // Cria um novo elemento <div> para a nova tarefa
+            let novaMensagem = document.createElement('div');
+            novaMensagem.innerText = mensagem; // Define o texto da nova tarefa como o valor inserido
+            novaMensagem.id = 'mensagem-' + contador; // Atribui um ID exclusivo à nova tarefa
+ 
+            // Adiciona a nova tarefa à DIV de tarefas
+            divMensagens.appendChild(novaMensagem);
+            // Limpa o campo de entrada após adicionar a tarefa
+            document.getElementById('mensager').value = '';
+        }
+    };
+});
+
+// [TROCANDO COR DO SITE]
+
+'use strict'
+
+const mudando= document.querySelector('.botão');
+
+mudando.addEventListener('click',function(){
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className=='light-theme') {
+        this.textContent = "Dark";
+    }
+    else{
+        this.textContent = "Light";
+    }
+    console.log('currente class name:' + className);
+});
+
+
